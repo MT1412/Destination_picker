5d279963a512015329e438b67f64301dd987c0fc
Martin Timmerman <77270706+MT1412@users.noreply.github.com>
2022-03-29 13:55:14 +0000

    added main routes

--- destination-picker/Web/View/Static/Calculate.hs
+++ destination-picker/Web/View/Static/Calculate.hs
@@ -7,13 +7,14 @@ instance View CalculateView where
     html CalculateView {wayThereValues, wayBackValues}= [hsx|
         <div>
             <p>
-            Hi there, this is your current time estimates in hours:
+            Hi there, these are your current time estimates in hours:
+            [routeWoudsend, routeSloten, routeLangweer, routeJoure, routeSneek, routeHeeg]
             </p>
             <div>
-                The time estimates for all routes to get to the destination [Woudsend, dummy route]: {wayThereValues}
+                The time estimates for all routes to get to the destination: {wayThereValues}
             </div>
             <div>
-                The time estimates for all routes to get back [Woudsend, dummy route]: {wayBackValues}
+                The time estimates for all routes to get back: {wayBackValues}
             </div>
         </div>
 |]
