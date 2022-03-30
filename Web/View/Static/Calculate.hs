f7b4bb3fc39f27455ca5910420b14962a445c0cd
Martin Timmerman <77270706+MT1412@users.noreply.github.com>
2022-03-30 13:34:47 +0000

    added route images

--- Web/View/Static/Calculate.hs
+++ Web/View/Static/Calculate.hs
@@ -14,6 +14,9 @@ getTotalSailTime (_, _, _, totalSailTime, _) = totalSailTime
 getLunchTime :: RouteInfo -> Float
 getLunchTime (_, _, _, _, lunchTime) = lunchTime
 
+getSrc :: RouteInfo -> String
+getSrc route = "/routeImages/" ++ (getRouteName route) ++ ".png"
+
 data CalculateView = CalculateView {calculatedRoutes :: [(String, Float, Float, Float, Float)]}
 
 instance View CalculateView where
@@ -31,10 +34,15 @@ instance View CalculateView where
 renderRoute :: RouteInfo -> Html
 renderRoute route = [hsx|
     <div class="routeinfoblock">
-        <h2>{getRouteName route}</h2>
-        <p>The way there will take {getWayThereTime route} hours. </p>
-        <p>You will arrive there around {getLunchTime route}. </p>
-        <p>The way back will take {getWayBackTime route} hours. </p>
-        <p>The total sail time will be around {getTotalSailTime route} hours. </p>
+        <h2 class="routetitle">{getRouteName route}</h2>
+        <div class="routeinfocontent">
+            <div class="routeinfotext">
+                <p>The way there will take {getWayThereTime route} hours. </p>
+                <p>You will arrive there around {getLunchTime route}. </p>
+                <p>The way back will take {getWayBackTime route} hours. </p>
+                <p>The total sail time will be around {getTotalSailTime route} hours. </p>
+            </div>
+            <img class="routeimage" src={getSrc route}>
+        </div>   
     </div>
 |]
